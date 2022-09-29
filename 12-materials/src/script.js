3e00955c90403c4341a88226c7b86f04d2bb2476
Daniel <mrdaniel19996@gmail.com>
2022-09-29 09:24:13 -0500

    class 13

---
+++ 12-materials/src/script.js
@@ -0,0 +1,215 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+
+//DEBUG
+
+const gui = new dat.GUI();
+
+/**
+ * Base
+ */
+const textureLoader = new THREE.TextureLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+const colorTextureDoor = textureLoader.load("/textures/door/color.jpg");
+const alphaTextureDoor = textureLoader.load("/textures/door/alpha.jpg");
+const heightTextureDoor = textureLoader.load("/textures/door/height.jpg");
+const normalTextureDoor = textureLoader.load("/textures/door/normal.jpg");
+const metalnessTextureDoor = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTextureDoor = textureLoader.load("/textures/door/roughness.jpg");
+const ambientOcclusionTextureDoor = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+
+const matcapTexture = textureLoader.load("/textures/matcaps/3.png");
+const gradientTexture = textureLoader.load("/textures/gradients/5.jpg");
+gradientTexture.minFilter = THREE.NearestFilter;
+gradientTexture.magFilter = THREE.NearestFilter;
+gradientTexture.generateMipmaps = false;
+
+const environmentMapText = cubeTextureLoader.load([
+  "/textures/environmentMaps/3/px.jpg",
+  "/textures/environmentMaps/3/nx.jpg",
+  "/textures/environmentMaps/3/py.jpg",
+  "/textures/environmentMaps/3/ny.jpg",
+  "/textures/environmentMaps/3/pz.jpg",
+  "/textures/environmentMaps/3/nz.jpg",
+]);
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+
+// const material = new THREE.MeshBasicMaterial();
+// material.map = colorTextureDoor;
+// material.color = new THREE.Color("pink");
+// material.wireframe = true;
+// material.opacity = 0.5;
+// material.transparent = true;
+// material.alphaMap = alphaTextureDoor;
+// material.side = THREE.DoubleSide;
+
+// const material = new THREE.MeshNormalMaterial();
+// material.flatShading = true;
+
+// const material = new THREE.MeshMatcapMaterial();
+// material.matcap = matcapTexture;
+
+//const material = new THREE.MeshDepthMaterial();
+
+// const material = new THREE.MeshPhongMaterial();
+// material.shininess = 100;
+// material.specular = new THREE.Color(0xff0000);
+
+// const material = new THREE.MeshToonMaterial();
+// material.gradientMap = gradientTexture;
+
+// const material = new THREE.MeshStandardMaterial();
+// material.metalness = 0.45;
+// material.roughness = 0.45;
+// material.map = colorTextureDoor;
+// material.aoMap = ambientOcclusionTextureDoor;
+// material.aoMapIntensity = 1;
+// material.displacementMap = heightTextureDoor;
+// material.displacementScale = 0.05;
+// material.metalnessMap = metalnessTextureDoor;
+// material.roughnessMap = roughnessTextureDoor;
+// material.normalMap = normalTextureDoor;
+// material.normalScale.set(0.5, 0.5);
+// material.transparent = true;
+// material.alphaMap = alphaTextureDoor;
+
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.7;
+material.roughness = 0.2;
+material.envMap = environmentMapText;
+
+gui.add(material, "metalness").min(0).max(1).step(0.0001);
+gui.add(material, "roughness").min(0).max(1).step(0.0001);
+gui.add(material, "aoMapIntensity").min(0).max(5).step(0.0001);
+gui.add(material, "displacementScale").min(0).max(1).step(0.0001);
+
+// LIGHTS
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material);
+sphere.position.x = 1;
+
+sphere.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+);
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material);
+plane.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 64, 128),
+  material
+);
+
+torus.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
+);
+
+torus.position.x = -1;
+
+scene.add(sphere, plane, torus);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update Objects
+  //   sphere.rotation.y = 0.1 * elapsedTime;
+  //   plane.rotation.y = 0.1 * elapsedTime;
+  //   torus.rotation.y = 0.1 * elapsedTime;
+
+  //   sphere.rotation.x = 0.15 * elapsedTime;
+  //   plane.rotation.x = 0.15 * elapsedTime;
+  //   torus.rotation.x = 0.15 * elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
